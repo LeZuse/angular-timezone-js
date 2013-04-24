3608487cf4bb9edccc4e3cf3fae419360dc308d1
michaelahlers <michael@ahlers.co>
2013-04-24 17:01:50 -0400

    A valid test, but failing.

--- test/directives/tzDateSpec.js
+++ test/directives/tzDateSpec.js
@@ -13,13 +13,36 @@ describe('tzDate', function () {
     $sandbox = $('<div id="sandbox"></div>').appendTo($('body'))
   }))
 
+  var compile = function (scenario) {
+    scenario = scenarios[scenario]
+    angular.extend(scope, scenario.scope)
+
+    var $element = $(scenario.element).appendTo($sandbox)
+
+    $element = $compile($element)(scope)
+    scope.$digest()
+
+    return $element
+  }
+
   afterEach(function () {
     $sandbox.remove()
     scope.$destroy()
   })
 
-  it('should work', function () {
-    expect(true).toBe(true)
+  var scenarios = {
+    'America/Chicago' : {
+      scope : {
+        reference : new Date(Date.parse('1970-01-01T00:00:00+00:00')),
+        timezone : 'America/Chicago'
+      },
+      element : '<span>{{reference|tzDate:timezone|date:"yyyy-MM-dd HH:mm:ss Z"}}</span>'
+    }
+  }
+
+  it('should format reference date to America/Chicago', function () {
+    var el = compile('America/Chicago')
+    expect(el.text()).toEqual('1969-12-31 18:00:00 -0600')
   })
 
 })
